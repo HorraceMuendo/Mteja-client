17b51c7c85b1a93977bdbab118065cc676ee3de8
Horrace Muendo <horracemuendo34@gmail.com>
2024-09-05 23:54:16 +0300

    A few changes here and there

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -30,16 +30,21 @@
 
 // src/components/Header.js
 import React from 'react';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav,  } from 'react-bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../Styles/Header.css';
+import logo from '../Images/logo.jpg' 
 
 function Header() {
   return (
-    <Navbar bg="light" expand="lg" className="header fixed-top justify-content-end" >
-      <Navbar.Brand href="#home">MySite</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
+    <Navbar bg="light" expand="lg" className="header fixed-top" >
+      {/* <Navbar.Brand href="#home">MySite</Navbar.Brand> */}
+      {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+      <Navbar.Brand href="/" className="d-flex align-items-center">
+        <img src={logo} alt="Logo" className="logo-image d-inline-block align-top" />
+        <span className="d-none d-lg-block ml-2">NaivasCRM</span>
+      </Navbar.Brand>
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
         <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#complaints">Complaints</Nav.Link>

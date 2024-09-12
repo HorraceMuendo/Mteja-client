15b2e7392c0c64384a5a10e5e29bee5b21a051be
Horrace Muendo <horracemuendo34@gmail.com>
2024-09-12 14:18:34 +0300

    added the browser router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
@@ -6,10 +7,21 @@ import Products from './Pages/Products';
 
 function App() {
   return (
-    <div className="App">
-    <Header/>
-    <Products/>
-    </div>
+
+    <Router>
+    <Header />
+    <Routes>
+      {/* <Route path="/" element={<Home />} /> Replace with your Home component */}
+      <Route path="/products" element={<Products />} />
+      {/* Add more routes here as needed */}
+      {/* <Route path="/contact" element={<Contact />} /> Example route */}
+    </Routes>
+  </Router>
+
+    // <div className="App">
+    // <Header/>
+    // <Products/>
+    // </div>
   );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,25 +1,57 @@
 
+// import React from 'react';
+// import { Navbar, Nav,  } from 'react-bootstrap';
+// import 'bootstrap-icons/font/bootstrap-icons.css';
+// import '../Styles/Header.css';
+// import logo from '../Images/logo.jpg' 
+
+// function Header() {
+//   return (
+//     <Navbar bg="light" expand="lg" className="header fixed-top" >
+//       <Navbar.Brand href="/" className="d-flex align-items-center">
+//         <img src={logo} alt="Logo" className="logo-image d-inline-block align-top" />
+//         <span className="d-none d-lg-block ml-2">NaivasCRM</span>
+//       </Navbar.Brand>
+//       <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+//         <Nav className="mr-auto">
+//           <Nav.Link href="#home">Home</Nav.Link>
+//           <Nav.Link href="/products">Products</Nav.Link>
+//           <Nav.Link href="#enquiries">Enquiries</Nav.Link>
+//           <Nav.Link href="#compliments">Compliments</Nav.Link>
+//           <Nav.Link href="#complaints">Complaints</Nav.Link>
+//           <Nav.Link href="#complaints">Contact Us</Nav.Link>
+//         </Nav>
+//       </Navbar.Collapse>
+//     </Navbar>
+//   );
+// }
+
+// export default Header;
+
+
+// src/components/Header.js
 import React from 'react';
-import { Navbar, Nav,  } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../Styles/Header.css';
-import logo from '../Images/logo.jpg' 
+import logo from '../Images/logo.jpg';
 
 function Header() {
   return (
-    <Navbar bg="light" expand="lg" className="header fixed-top" >
-      <Navbar.Brand href="/" className="d-flex align-items-center">
+    <Navbar bg="light" expand="lg" className="header fixed-top">
+      <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
         <img src={logo} alt="Logo" className="logo-image d-inline-block align-top" />
         <span className="d-none d-lg-block ml-2">NaivasCRM</span>
       </Navbar.Brand>
       <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#products">Products</Nav.Link>
-          <Nav.Link href="#enquiries">Enquiries</Nav.Link>
-          <Nav.Link href="#compliments">Compliments</Nav.Link>
-          <Nav.Link href="#complaints">Complaints</Nav.Link>
-          <Nav.Link href="#complaints">Contact Us</Nav.Link>
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
+          <Nav.Link as={Link} to="/products">Products</Nav.Link>
+          <Nav.Link as={Link} to="/enquiries">Enquiries</Nav.Link>
+          <Nav.Link as={Link} to="/compliments">Compliments</Nav.Link>
+          <Nav.Link as={Link} to="/complaints">Complaints</Nav.Link>
+          <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
@@ -27,5 +59,3 @@ function Header() {
 }
 
 export default Header;
-
-

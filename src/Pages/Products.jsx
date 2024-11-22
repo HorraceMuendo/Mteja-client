89247c8920281e35c6b0e4179896180322e285da
Horrace Muendo <horracemuendo34@gmail.com>
2024-11-22 10:12:14 +0300

    Made some few changes to product js

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,10 +1,49 @@
+// // src/pages/Products.js
+// import React, { useState, useEffect } from 'react';
+// import ProductCard from '../Components/ProductCard.js';
+// import { Container, Row, Col } from 'react-bootstrap';
+
+// function Products() {
+//   const [products, setProducts] = useState([]);
+
+//   useEffect(() => {
+//     // Replace with your API endpoint
+//     fetch('http://localhost:6969/products/list')
+//       .then(response => response.json())
+//       .then(data => setProducts(data))
+//       .catch(error => console.error('Error fetching products:', error));
+//   }, []);
+
+//   return (
+//     <Container className="mt-5">
+//       <h1 className="mb-4">Our Products</h1>
+//       <Row>
+//         {products.map(product => (
+//           <Col key={product.id} md={4}>
+//             <ProductCard 
+//               name={product.name}
+//               description={product.description}
+//               details={product.details}
+//             />
+//           </Col>
+//         ))}
+//       </Row>
+//     </Container>
+//   );
+// }
+
+// export default Products;
+
+
 // src/pages/Products.js
 import React, { useState, useEffect } from 'react';
 import ProductCard from '../Components/ProductCard.js';
+import TrackPageVisit from './TrackingPageVisits.jsx'; // Import the TrackPageVisit component
 import { Container, Row, Col } from 'react-bootstrap';
 
 function Products() {
   const [products, setProducts] = useState([]);
+  const customerId = 1; // This should be dynamically set based on the logged-in user
 
   useEffect(() => {
     // Replace with your API endpoint
@@ -20,6 +59,11 @@ function Products() {
       <Row>
         {products.map(product => (
           <Col key={product.id} md={4}>
+            <TrackPageVisit 
+              customerId={customerId} 
+              pageUrl={product.details} 
+              visitType="page_view" 
+            />
             <ProductCard 
               name={product.name}
               description={product.description}
